a2f8cd87f07ee8eaec85aad4136814718a0b4026
iamnotrodger <rodger.retanal24@gmail.com>
2020-01-06 13:18:30 -0500

    A non-working version with floats

--- calculate/calculate.ino
+++ calculate/calculate.ino
@@ -22,9 +22,9 @@ const byte COLS = 4;
 
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3', '+'},
-  {'4', '5', '6', '-'},
+  {'4', '5', '6', '/'},
   {'7', '8', '9', 'c'},
-  {'*', '0', '/', '='}
+  {'*', '0', '.', '='}
 };
 
 byte rowPins[ROWS] = {9, 8, 7, 6};
@@ -33,9 +33,6 @@ byte colPins[COLS] = {5, 4, 3, 2};
 /* Holds the value for User inputs*/
 float numOne;
 float total = 0;
-
-// array for printing it with a for loop, stores each num input
-char digit[7];
 
 /* operation */
 enum Operator {none, add = '+', subtract = '-', multiply = '*', divide = '/', clr = 'c', equal = '=', dot = '.', negative = '_'};
@@ -50,6 +47,7 @@ int numDigit = 0;
 /* if an operation has been entered */
 boolean hasResult = false;
 boolean hasError = false;
+boolean keepNum = false;
 
 // key character from key pad
 char key;
@@ -61,18 +59,13 @@ Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS)
 LedControl lc = LedControl(12, 11, 10, 1);
 
 /////////////////////////////////////////////////////////////////////////////////
-// Called if the CLEAR operator has been clicked
-void reset() {
-  numOne = 0;
-  total = 0;
-  numDigit = 0;
-  oper = none;
-  hasResult = false;
-}
 
 // Key Listener Functions
 void keyListener() {
   switch (key) {
+    case '0':
+      numKeySetter(0);
+      break;
     case '1':
       numKeySetter(1);
       break;
@@ -102,14 +95,19 @@ void keyListener() {
       break;
     case add:
       oper = add;
+      keepNum = true;
       break;
     case subtract:
       oper = subtract;
+      keepNum = true;
       break;
     case multiply:
       oper = multiply;
+      keepNum = true;
+      break;
     case divide:
       oper = divide;
+      keepNum = true;
       break;
     case clr:
       reset();
@@ -118,7 +116,7 @@ void keyListener() {
       getEqual();
       break;
     case dot:
-      setDecimal();
+      hasDecimal = true;
       break;
     case negative:
       total *= -1;
@@ -131,33 +129,37 @@ void keyListener() {
 
 // Adding the values inputed from the user
 void numKeySetter(float i) {
+  if (i == 0 && total == 0) {
+    return;
+  }
   // AHHHHHHH i can't explain this in words sooo yeahhhh.....
   if (hasResult && oper == none) {
     reset();
   }
 
   // This condition makes sure that the print doesn't get cleared until a new number input has been clicked.
-  if (oper != none) {
+  if (keepNum && oper != none) {
     numOne = total;
     total = 0;
+    numDigit = 0;
+    hasDecimal = false;
+    lc.clearDisplay(0);
   }
 
   if ( numDigit < 7) {
-    if (total == 0) {
+    if (hasDecimal) {
+      setDecimal(i);
+    } else if (total == 0) {
       total = i;
     } else {
       total = (total * 10) + i;
     }
 
-    digit[numDigit] = i;
     numDigit++;
   }
+
+  keepNum = false;
 }
-
-// TODO: implement adding decimals
-void setDecimal() {
-
-};
 
 // Called if the EQUAL operator has been clicked. This evaluates the total and the number one value
 void getEqual() {
@@ -177,19 +179,92 @@ void getEqual() {
           total = numOne / total;
         } else {
           hasError = true;
-          reset();
+          return;
         }
         break;
     }
     hasResult = true;
     oper = none;
+    //TODO: make is so this also calculates the decimal place 
+    numDigit = lenHelper(total);
   }
 }
+
+// Called if the CLEAR operator has been clicked
+void reset() {
+  numOne = 0;
+  total = 0;
+  numDigit = 0;
+  oper = none;
+  hasResult = false;
+  hasError = false;
+  hasDecimal = false;
+  lc.clearDisplay(0);
+  lc.setDigit(0, 0, 0, false);
+}
+
+//Finding the length of digit
+int lenHelper(float x) {
+  if (x < 0) {
+    x *= -1;
+  }
+//  if (x <= 0) return 0;
+//  if (x >= 1000000000) return 10;
+//  if (x >= 100000000)  return 9;
+//  if (x >= 10000000)   return 8;
+//  if (x >= 1000000)    return 7;
+//  if (x >= 100000)     return 6;
+//  if (x >= 10000)      return 5;
+//  if (x >= 1000)       return 4;
+//  if (x >= 100)        return 3;
+//  if (x >= 10)         return 2;
+//  return 1;
+  return log10(x) + 1;
+}
+
+// TODO: implement adding decimals
+// TODO: deal with this scenario : user inputs 0s as decimals 
+void setDecimal(float i) {
+  // Extract decimal from total
+  int intpart = (int) total;
+  float decimal = total - intpart;
+  int len = lenHelper(decimalToFloat(decimal));
+  
+  //Exponential function
+  if (len == 0) {
+    i /= 10;
+  } else if (len == 1) {
+    i /= 100;
+  } else if (len == 2) {
+    i /= 1000;
+  } else if (len == 3) {
+    i /= 10000;
+  } else if (len <= 4) {
+    i /= 100000;
+  }  else if (len <= 5) {
+    i /= 1000000;
+  } else if (len <= 6) {
+    i /= 10000000;
+  }
+  //  Serial.println(i);
+  total += i;
+};
+
+float decimalToFloat(float dec) {
+  float temp = dec;
+  while(abs(temp) <= 0.1) {
+    dec *= 10;
+    Serial.println(dec);
+    temp *=10;
+    temp -= (int)temp;
+  }
+  return dec;
+}
+
 
 
 //// Printing Helper
 float remainder (float val) {
-
   return (fmod(val, 10));
 }
 
@@ -211,116 +286,85 @@ void setup() {
   lc.setIntensity(0, 5);
   /* and clear the display */
   lc.clearDisplay(0);
+
+  lc.setDigit(0, 0, 0, false);
 }
 
 
 void printNumber(float v) {
 
-  boolean negative;
+  boolean negative = false;
 
-  if (v < -9999999 || v > 9999999)
+  if (numDigit == 0) {
+    lc.setDigit(0, 0, 0, false);
     return;
+  }
+
+  if (v < -9999999 || v > 9999999) {
+    return;
+  }
+
   if (v < 0) {
     negative = true;
     v *= -1;
   }
 
-  if (!negative) {
+  if (negative) {
     //print character '-' in the leftmost column
     lc.setChar(0, numDigit, '-', false);
   }
-//  else {
-//    //print a blank in the sign column
-//    lc.setChar(0, numDigit, ' ', false);
-//  }
 
-  float ones;
-  float tens;
-  float hundreds;
-  float thousands;
-  float tenthousands;
-  float hundthousands;
-  float millions;
+  lc.clearDisplay(0);
+  float temp = v;
+  //This extracts the decimal when it loops 
+  boolean extractDecimal = true;
+  int numberOfDecimal;
 
-  ones = remainder(v); v = v / 10;
-  tens = remainder(v); v = v / 10;
-  hundreds = remainder(v); v = v / 10;
-  thousands = remainder(v); v = v / 10;
-  tenthousands = remainder(v); v = v / 10;
-  hundthousands = remainder(v); v = v / 10;
-  millions = v;
-
-  /* prints the number digit by digit */
-  if (numDigit == 6) {
-    lc.setDigit(0, 6, (byte)millions, false);
-  } else {
-    lc.setDigit(0, 6, ' ', false);
-  }
-  if (numDigit >= 5) {
-    lc.setDigit(0, 5, (byte)hundthousands, false);
-  }
-  else {
-    lc.setDigit(0, 5, ' ', false);
-  }
-  if (numDigit >= 4) {
-    lc.setDigit(0, 4, (byte)tenthousands, false);
-  }
-  else {
-    lc.setDigit(0, 4, ' ', false);
-  }
-  if (numDigit >= 3) {
-    lc.setDigit(0, 3, (byte)thousands, false);
-  }
-  else {
-    lc.setDigit(0, 3, ' ', false);
-  }
-  if (numDigit >= 2) {
-    lc.setDigit(0, 2, (byte)hundreds, false);
-  }
-  else {
-    lc.setDigit(0, 2, ' ', false);
-  }
-  if (numDigit >= 1) {
-    lc.setDigit(0, 1, (byte)tens, false);
-  }
-  else {
-    lc.setDigit(0, 1, ' ', false);
-  }
-
-  lc.setDigit(0, 0, (byte)ones, false);
-
+  
   //Now print the number digit by digit
-//  char digit[7];
-//  int iterator = numDigit - 1;
-//  for (int i = 0; i < 7; i++) {
-//    if (i > 0 && i <= numDigit) {
-//      lc.setDigit(0, i, (byte) digit[iterator--], false);
-//     } else {
-//      lc.setDigit(0, i, ' ', false);
-//    }
-//  }
+  for (int i = 0; i < numDigit; i++) {
+    temp = remainder(v);
+    v = v / 10;
+    if(i < numDigit - 1) {
+      lc.setDigit(0, i, (byte)temp, false);
+//      if(extractDecimal && hasDecimal) {
+//         int intpart = (int) total;
+//         float decimal = total - intpart;
+//         v = decimal;
+//         extractDecimal = false;
+//      }
+    } else {
+      lc.setDigit(0, i, (byte)temp, hasDecimal);
+    }
+  }
 }
 
-// TODO: print an error message when an arithmetic error appears
 void printError() {
-
+  lc.clearDisplay(0);
+  //REEEEEEEEEEEEEEEEEEE PICKLE RICK 
+  lc.setChar(0, 7, 'E', false);
+  lc.setChar(0, 6, 'E', false);
+  lc.setChar(0, 5, 'E', false);
+  lc.setChar(0, 4, 'E', false);
+  lc.setChar(0, 3, 'E', false);
+  lc.setChar(0, 2, 'E', false);
+  lc.setChar(0, 1, 'E', false);
+  lc.setChar(0, 0, 'E', false);
 }
 
 
 void loop() {
   //  writeArduinoOn7Segment();
   key = customKeypad.getKey();
-
   if (key) {
     keyListener();
     Serial.println(key);
     Serial.println(total);
-    Serial.println(numDigit);
     if (!hasError) {
-    printNumber(total);
-  } else {
-    printError();
-  }
+      printNumber(total);
+    } else {
+      printError();
+    }
   }
 
 
